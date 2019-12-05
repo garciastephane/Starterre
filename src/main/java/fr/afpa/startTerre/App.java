c0e97deba579daffe0c05811c8da9a71b73cb76d
Fayaz <fayaz.akbaraly@gmail.com>
2019-12-05 09:29:44 +0100

    FA_Main_App

--- src/main/java/fr/afpa/startTerre/App.java
+++ src/main/java/fr/afpa/startTerre/App.java
@@ -1,13 +1,26 @@
 package fr.afpa.startTerre;
 
+import javax.swing.JFrame;
+
+import fr.afpa.ihm.Scene;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	public static Scene scene;
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	JFrame window = new JFrame("Star Terre");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(500, 800);
+		window.setLocationRelativeTo(null);
+		
+		
+		scene = new Scene();
+		window.setContentPane(scene);
+		window.setVisible(true);
     }
 }
