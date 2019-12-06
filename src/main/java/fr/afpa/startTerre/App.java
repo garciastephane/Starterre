88c25bfce90486d2bd22907224241c941160d080
Stephane <garciastephan@hotmail.com>
2019-12-06 10:21:06 +0100

    SG_Menu_Jeu

--- src/main/java/fr/afpa/startTerre/App.java
+++ src/main/java/fr/afpa/startTerre/App.java
@@ -1,28 +1,31 @@
 package fr.afpa.startTerre;
-
 import javax.swing.JFrame;
-
+import javax.swing.JOptionPane;
+import fr.afpa.ihm.LoginIhm;
 import fr.afpa.ihm.Scene;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+public class App {
 	public static Scene scene;
-    public static void main( String[] args )
-    {
-    	JFrame window = new JFrame("Star Terre");
+	public static LoginIhm loginihm;
+
+	public static void main(String[] args) {
+
+		JOptionPane jop = new JOptionPane(), jop2 = new JOptionPane();
+		String nom = JOptionPane.showInputDialog(null, "Veuillez entrer votre nom !", "Bienvenue sur STARTERRE",
+				JOptionPane.QUESTION_MESSAGE);
+		JOptionPane.showMessageDialog(null, "Votre nom est " + nom, "Identité", JOptionPane.INFORMATION_MESSAGE);
+
+		JFrame window = new JFrame("Star Terre");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setSize(500, 800);
 		window.setResizable(false);
 
 		window.setLocationRelativeTo(null);
-		
-		
+
 		scene = new Scene();
 		window.setContentPane(scene);
 		window.setVisible(true);
-    }
+
+	}
+
 }
