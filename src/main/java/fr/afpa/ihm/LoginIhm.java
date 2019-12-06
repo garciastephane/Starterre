fe8f2a07d998e9e70b197cf935533d57483ea14b
Stephane <garciastephan@hotmail.com>
2019-12-06 11:08:49 +0100

    SG_Correction_Menu_Jeu

--- src/main/java/fr/afpa/ihm/LoginIhm.java
+++ src/main/java/fr/afpa/ihm/LoginIhm.java
@@ -1,5 +1,27 @@
 package fr.afpa.ihm;
 
+import javax.swing.JOptionPane;
+
+import fr.afpa.controles.PlayerControl;
+
 public class LoginIhm {
 
+	public static void lancement() {
+
+		JOptionPane jop = new JOptionPane(), jop2 = new JOptionPane();
+		String nom;
+				
+do {
+	nom = JOptionPane.showInputDialog(null, "Veuillez entrer votre nom !", "Bienvenue sur STARTERRE",
+			JOptionPane.QUESTION_MESSAGE);
+		if (PlayerControl.validNamePlayer(nom)) {
+			JOptionPane.showMessageDialog(null, "Votre nom est " + nom, "Identité", JOptionPane.INFORMATION_MESSAGE);
+		}
+		
+		else  {
+			JOptionPane.showMessageDialog(null, "Veuillez entrer un nom valable" , "Identite", JOptionPane.INFORMATION_MESSAGE);
+		}
+
+	} while (!PlayerControl.validNamePlayer(nom));
+	}
 }
--- src/main/java/fr/afpa/startTerre/App.java
+++ src/main/java/fr/afpa/startTerre/App.java
@@ -1,6 +1,5 @@
 package fr.afpa.startTerre;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import fr.afpa.ihm.LoginIhm;
 import fr.afpa.ihm.Scene;
 
@@ -10,11 +9,8 @@ public class App {
 
 	public static void main(String[] args) {
 
-		JOptionPane jop = new JOptionPane(), jop2 = new JOptionPane();
-		String nom = JOptionPane.showInputDialog(null, "Veuillez entrer votre nom !", "Bienvenue sur STARTERRE",
-				JOptionPane.QUESTION_MESSAGE);
-		JOptionPane.showMessageDialog(null, "Votre nom est " + nom, "Identité", JOptionPane.INFORMATION_MESSAGE);
-
+		LoginIhm.lancement();
+		
 		JFrame window = new JFrame("Star Terre");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setSize(500, 800);
